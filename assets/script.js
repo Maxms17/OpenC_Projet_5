3c20438970d2f7b6f0f47312476d21216b215d6e
Maxms17 <61410393+Maxms17@users.noreply.github.com>
2023-04-03 11:19:50 +0200

    Modification image

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,49 @@
 const prevButton = document.getElementById("left");
 const nextButton = document.getElementById("right");
 const dots = document.getElementById("dots");
-
-const nb_dot = sequence.length;
-const img = slide.map(slide => slide.image);
-const tag = slide.map(slide => slide.tagLine);
-
+const banner = document.getElementById("banner");
+	
 const slide = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
-
+	
+const nb_dot = slide.length;
+const img = slide.map(slide => slide.image);
+const tag = slide.map(slide => slide.tagLine);
+	
+let currentImage = 0
+	
 prevButton.addEventListener("click", () => {
 	alert("left");
 });
-  
-nextButton.addEventListener("click", () => {
-	alert("right");
-});
-
-document.body.appendChild(banner) = '<div>'
-    + '<img src="' + slide.image + '" />'
-    + '<h2>' + slide.tagLine + '</h2>'
-    + '</div>'
 	
+nextButton.addEventListener("click", () => {
+	// alert("right");
+	currentImage = currentImage + 1
+	test()
+});
+	
+const node = document.createElement('div')
+	
+function test() {
+	node.innerHTML = '<img src="' + "./assets/images/slideshow/" + img[currentImage] + '" />'
+		+ '<h2>' + slide.tagLine + '</h2>'
+	banner.appendChild(node)
+}
+	
+test()
