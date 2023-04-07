f885856d7796845a02ebd63f74b649b7ef25b8bb
Maxms17 <61410393+Maxms17@users.noreply.github.com>
2023-04-07 09:57:34 +0200

    Ajout changement dot selectionné

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,14 @@
 const prevButton = document.getElementById("left");
 const nextButton = document.getElementById("right");
 
+const img_banner= document.getElementById("img_banner")
+const text_banner= document.getElementById("text_banner")
+
+const dot_un= document.getElementById("dot_un")
+const dot_deux= document.getElementById("dot_deux")
+const dot_trois= document.getElementById("dot_trois")
+const dot_quatre= document.getElementById("dot_quatre")
+
 const slide = [
 	{
 		"image": "slide1.jpg",
@@ -37,6 +45,7 @@ prevButton.addEventListener("click", () => {
 		currentTag = currentTag - 1
 	}
 	changeBanniere()
+	changeDot()
 });
 	
 nextButton.addEventListener("click", () => {
@@ -49,14 +58,40 @@ nextButton.addEventListener("click", () => {
 		currentTag = currentTag + 1
 	}
 	changeBanniere()
+	changeDot()
 });
-
-const img_banner= document.getElementById("img_banner")
-const text_banner= document.getElementById("text_banner")
 
 function changeBanniere(){
     //img_banner.src = "' + "./assets/images/slideshow/" + img[currentImage] + '"
 	text_banner.innerHTML = tag[currentTag]
 }
 
+function changeDot(){
+    if (img[currentImage] == 0){
+		dot_un.class = "dot dot_selected"
+		dot_deux.class = "dot"
+		dot_trois.class = "dot"
+		dot_quatre.class = "dot"
+	}
+	else if (img[currentImage] == 1){
+		dot_un.class = "dot"
+		dot_deux.class = "dot dot_selected"
+		dot_trois.class = "dot"
+		dot_quatre.class = "dot"
+	}
+	else if (img[currentImage] == 2){
+		dot_un.class = "dot"
+		dot_deux.class = "dot"
+		dot_trois.class = "dot dot_selected"
+		dot_quatre.class = "dot"
+	}
+	else if (img[currentImage] == 3){
+		dot_un.class = "dot"
+		dot_deux.class = "dot"
+		dot_trois.class = "dot"
+		dot_quatre.class = "dot dot_selected"
+	}
+}
+
 changeBanniere()
+changeDot()
